6158a0d51ccec32dc8406ea8de2f82890ac42df0
Tristan Fox <tristanfox009@outlook.com>
2022-10-31 07:20:10 +1100

    Re-allow $.text in command arguments

--- grammar.js
+++ grammar.js
@@ -58,6 +58,7 @@ module.exports = grammar({
                     ),
                     $.variable,
                     $.primitive,
+                    $.text,
                 )
             )),
         ),
